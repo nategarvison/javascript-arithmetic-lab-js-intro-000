f80a3087d261fa44488f349c7a7dbaff4811af4a
nategarvison <ngarvison@gmail.com>
2017-08-22 16:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function makeInt(n) {
   return parseInt(n);
 }
 function makeInt(n) {
-  return parseIn(n, 10);
+  return parseInt(n, 10);
 }
 function preserveDecimal(n) {
   return parseFloat(n, 10);
